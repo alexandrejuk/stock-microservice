d84047da5c9d60f19dfad6e907299d71ab5b2f9a
alexandrejuk <ale_santos.soares@hotmail.com>
2018-10-25 17:07:43 -0300

    domains: created index exports to domains

---
+++ src/domains/index.js
@@ -0,0 +1,10 @@
+const productDomain = require('./Product')
+const stockDomain = require('./Stock')
+const stockLocationDomain = require('./StockLocation')
+
+module.exports = {
+  productDomain,
+  stockDomain,
+  stockLocationDomain,
+  
+}
