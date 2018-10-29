dd820c48f0ad5cdc357bbd7fea8cc29eb9f18434
alexandrejuk <ale_santos.soares@hotmail.com>
2018-10-29 15:18:17 -0300

    chore: add import of order domains

--- src/domains/index.js
+++ src/domains/index.js
@@ -1,10 +1,12 @@
 const ProductDomain = require('./Product')
 const StockDomain = require('./Stock')
 const StockLocationDomain = require('./StockLocation')
+const OrderDomain = require('./Order')
 
 module.exports = {
   ProductDomain,
   StockDomain,
   StockLocationDomain,
+  OrderDomain,
 
 }
