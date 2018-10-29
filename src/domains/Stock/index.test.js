8b26c370970fd76fefb06e874c2bb24cc920d8fe
alexandrejuk <ale_santos.soares@hotmail.com>
2018-10-29 15:19:06 -0300

    domains: add product  and stock location mock

--- src/domains/Stock/index.test.js
+++ src/domains/Stock/index.test.js
@@ -6,16 +6,29 @@ const databaseHelper = require('../../helpers/database')
 
 const stockDomain = new Stock()
 
+const productMock = {
+  name: 'iphone 7s',
+  brand: 'apple',
+  sku: 'a1v456578',
+  category: 'cell phone',
+  serialControl: true,
+  priceBuy: 89000,
+  priceSell: 123000
+}
+
+const stockLocationMock = { name: 'amazon' }
+
+
 test.before(databaseHelper.isDatabaseConnected)
 
 test.beforeEach(async t => {
   const stockLocationDomain = new StockLocationDomain()
   const productDomain = new ProductDomain()
 
-  const stockLocationData = { name: 'amazon' }
+  const stockLocationData = stockLocationMock
   const stockLocation = await stockLocationDomain.add(stockLocationData)
 
-  const productData = { name: 'amazon' }
+  const productData = productMock
   const product = await productDomain.add(productData)
 
   t.context = {}
@@ -37,30 +50,8 @@ test('should be add a new event on stock', async t => {
   t.is(stockData.quantity, createdStock.quantity)
 })
 
-// test('add should return a validation error if product or stock does not exists', async t => {
-//   const stockData = { stockLocationId: null, productId: null, quantity: 10 }
-
-//   await t.throws(async () => {
-// 		await stockDomain.add(stockData)
-// 	}, {instanceOf: ValidationError, message: null})
-// }) 
-
-const fn = () => {
-	throw new TypeError('🦄');
-};
-
-test('throws', t => {
-	const error = t.throws(() => {
-		fn();
-	}, TypeError);
-
-	t.is(error.message, '🦄');
-});
-
-
-test('rejects', async t => {
+test('should not add stock if stock location or product id dont exist', async t => {
   const stockData = { stockLocationId: null, productId: null, quantity: 10 }
-
   const error = await t.throws(stockDomain.add(stockData))
 
 	t.true(error instanceof ValidationError)
