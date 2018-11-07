9a09c482c8d181253c13d022186e6db7fef01488
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-11-07 02:41:27 -0200

    database: add product, stock and stock-location migration files

---
+++ src/db/migrations/20181107042951-create-stock-location-table.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('stockLocation', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,  
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('stockLocation'),
+}
