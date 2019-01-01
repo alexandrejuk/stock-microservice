5a10c9ee54c3f56eb62c4cc0919557b1ebca665f
vitorsilvalima <vitorsilvalima29@gmail.com>
2019-01-01 12:03:25 -0200

    app: fix database connection settings

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const models = require('./models')
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DATABASE_HOST || 'localhost',
   dialect: 'postgres',
   operatorsAliases: false,
